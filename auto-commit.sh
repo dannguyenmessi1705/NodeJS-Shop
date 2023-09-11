eeb08deff25f1f489ad39224913bd5ca826d145f
Di Đan Nguyễn <didannguyen17@gmail.com>
2023-09-11 13:00:01 +0000

    Auto commit on 2023-09-11 13:00:01

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 cd /root/NodeJS-Shop/
 
 git config --global user.email "didannguyen17@gmail.com"
 git config --global user.name "Di Đan Nguyễn"
+
 # # Pull the latest changes from the remote repository
 git pull origin main
 
