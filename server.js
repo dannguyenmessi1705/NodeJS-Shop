a8c5ecd9885d7a845b20f6d3f5cc014539738306
dannguyenmessi1705 <messiprohy@gmail.com>
2023-10-02 13:34:30 +0700

    FIX HELMET, ACCEPT API GOOGLE AND VNPAY Header Request

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(
     directives: { // Các directives cho phép bạn chỉ định các nguồn tài nguyên nào được load trên trang web của bạn
       defaultSrc: ["'self'"], // Chỉ cho phép load các nguồn tài nguyên từ chính domain của bạn
       frameSrc: ["'self'", 'https://www.google.com/'], // Chỉ cho phép load các nguồn tài nguyên từ chính domain của bạn và từ https://www.google.com/ (ví dụ: iframe)
+      formAction: ["'self'", "https://sandbox.vnpayment.vn/"], // Chỉ cho phép load các nguồn tài nguyên từ chính domain của bạn và từ https://sandbox.vnpayment.vn/ (ví dụ: form)
+      connectSrc: ["'self'", "https://sandbox.vnpayment.vn/"], // Chỉ cho phép load các nguồn tài nguyên từ chính domain của bạn và từ https://sandbox.vnpayment.vn/ (ví dụ: fetch, xhr, websocket)
     },
   })
 );
